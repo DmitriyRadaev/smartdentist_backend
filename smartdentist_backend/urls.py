54b1eb60a6a0c98305d85b249db88e5a2a79fe83
DmitriyRadaev <142414012+DmitriyRadaev@users.noreply.github.com>
2026-01-23 15:14:05 +0400

    Авторизация/Регистрация/Модель пользователя

---
+++ smartdentist_backend/urls.py
@@ -0,0 +1,40 @@
+"""
+URL configuration for smartdentist_backend project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.views.static import serve
+from django.conf import settings
+from main import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #Авторизация
+    path("api/auth/login/", views.loginView, name="login"),
+    path("api/auth/logout/", views.logoutView, name="logout"),
+    path("api/auth/refresh_token/", views.CookieTokenRefreshView.as_view(), name="token_refresh"),
+
+    # Регистрация
+    path("api/auth/register/worker/", views.WorkerRegisterView.as_view(), name="worker_register"),
+    path("api/auth/register/admin/", views.AdminRegisterView.as_view(), name="admin_register"),
+
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
